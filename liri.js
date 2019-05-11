87ccd9ba4e377fef9643aed30aaf585224d243e2
JonP <jp17_2010@yahoo.com>
2019-05-11 08:12:20 -0500

    Finished function to call OMBD API

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+//var keys = require("./keys.js");
+var axios = require("axios");
+//var spotify = new Spotify(keys.spotify);
 
 var action = process.argv[2];
 var value = process.argv[3];
@@ -22,7 +23,7 @@ case "do-what-it-says":
 };
 
 function concert(){
-
+    
 };
 
 function spotify(){
@@ -30,7 +31,19 @@ function spotify(){
 };
 
 function movie(){
-
+    var queryUrl = "http://www.omdbapi.com/?t=" + value + "&imbdRating=&Ratings=&Title=&Actors=&Country=&Language=&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrl).then(
+        function(response) {
+            console.log("Movie Title: " + response.data.Title)
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMBD Rating: " + response.data.imbdRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[2].Value);
+            console.log("Country Produced In: " + response.data.Country)
+            console.log("Original Language: " + response.data.Language)
+            console.log("Movie Plot: " + response.data.Plot)
+            console.log("Actors: " + response.data.Actors)
+        }
+      );
 };
 
 function whatever(){
