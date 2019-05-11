1d975f8eab76a829984ccc24ac419e44ee88d826
JonP <jp17_2010@yahoo.com>
2019-05-11 17:00:13 -0500

    created concert function

--- liri.js
+++ liri.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 
 //var keys = require("./keys.js");
 var axios = require("axios");
+var moment = require('moment');
 //var spotify = new Spotify(keys.spotify);
 
+var nodeArgs = process.argv;
 var action = process.argv[2];
-var value = process.argv[3];
 
 switch(action){
 case "concert-this":
@@ -23,15 +24,67 @@ case "do-what-it-says":
 };
 
 function concert(){
-    
+    var artist = "";
+    var divider = "--------------------------";
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+          artist = artist + "+" + nodeArgs[i];
+        }
+        else {
+          artist += nodeArgs[i];      
+        }
+      }
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    axios.get(queryURL).then(
+        function(response){
+            for(var j = 0; j < response.data.length; j++){
+                console.log(response.data[j].venue.name);
+                console.log(response.data[j].venue.city + ", " + response.data[j].venue.region + ", " + response.data[j].venue.country);
+                var datetime = response.data[j].datetime.split("T");
+                var date = moment(`${datetime[0]}`, "YYYY-MM-DD")
+                console.log("Date:" + date.format("L"));
+                console.log(divider);
+            }
+        }
+    )
+
 };
 
 function spotify(){
+    var songName = "";
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+          songName = songName + "+" + nodeArgs[i];
+        }
+        else {
+          songName += nodeArgs[i];      
+        }
+    }
+
+    if (songName === ""){
+        songName = "The Sign";
+    }
 
 };
 
-function movie(){
-    var queryUrl = "http://www.omdbapi.com/?t=" + value + "&imbdRating=&Ratings=&Title=&Actors=&Country=&Language=&y=&plot=short&apikey=trilogy";
+function movie(){ 
+    var movieName = "";
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+          movieName = movieName + "+" + nodeArgs[i];
+        }
+        else {
+          movieName += nodeArgs[i];      
+        }
+    }
+
+      if(movieName ===""){
+        movieName = "Mr.Nobody";
+        };
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&imbdRating=&Ratings=&Title=&Actors=&Country=&Language=&y=&plot=short&apikey=trilogy";
+
     axios.get(queryUrl).then(
         function(response) {
             console.log("Movie Title: " + response.data.Title)
