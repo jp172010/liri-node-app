bb9d5fd5296c3454cd645f6b9a4b3cdd0aace667
JonP <jp17_2010@yahoo.com>
2019-05-11 21:59:04 -0500

    finished product

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # liri-node-app
+
+## Overview
+
+Liri-node-app is an application that takes in arguments from the bash/terminal and returns the corresponding data.
+
+You can use Liri to: search for upcoming concerts by artist name, search for movie info by movie name, search for song info by song name, or do something random.
+
+## How to use:
+
+To get started open the Liri-node-app depository in your bash/terminal.
+    Type "Node liri.js" followed by one of these four commands:
+        Search for upcoming concerts: "concert-this <artist name>", 
+        Search songs: "spotify-this-song <song name>",
+        Search movies: "movie-this <movie name>",
+        Do something random: "do-what-it-says"
+
+If you run into any problems feel free to contact me https://jp172010.github.io/.
+
+I am the only programmer maintaining/contributing to this project.
+
+## Examples:
+
+### Step 1: Using your bash/terminal find the liri-node-app directory
+/liri-node-app/images/Change Directory.png
+/liri-node-app/images/Inside the liri-node-app depository.png
+
+### Step 2: Type in "node liri.js"
+/liri-node-app/images/"node liri.js".png
+
+### Step 3: Pick a command (concert-this, spotify-this-song, movie-this, do-what-it-says)
+/liri-node-app/images/movie-this command.png
+/liri-node-app/images/concert-this command.png
+/liri-node-app/images/spotify-this-song command.png
+/liri-node-app/images/do-what-is-says command.png
+
+### Step 4: Add in a search query(artist, song, movie)
+/liri-node-app/images/search query movie.png
+/liri-node-app/images/search query artist.png
+/liri-node-app/images/search query song.png
+
+### Step 5: Enjoy!
+/liri-node-app/images/movie full search.png
+/liri-node-app/images/concert full search.png
+/liri-node-app/images/song full search.png
+/liri-node-app/images/do something random.png
--- liri.js
+++ liri.js
@@ -1,104 +1,235 @@
 require("dotenv").config();
 
-//var keys = require("./keys.js");
+var keys = require("./keys.js");
 var axios = require("axios");
-var moment = require('moment');
-//var spotify = new Spotify(keys.spotify);
-
-var nodeArgs = process.argv;
+var fs = require("fs");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var divider = "--------------------------";
 var action = process.argv[2];
 
-switch(action){
-case "concert-this":
+switch (action) {
+  case "concert-this":
     concert();
     break;
-case "spotify-this-song":
-    spotify();
+  case "spotify-this-song":
+    song();
     break;
-case "movie-this":
+  case "movie-this":
     movie();
     break;
-case "do-what-it-says":
+  case "do-what-it-says":
     whatever();
     break;
-};
+}
 
-function concert(){
-    var artist = "";
-    var divider = "--------------------------";
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-          artist = artist + "+" + nodeArgs[i];
+function concert() {
+  var artist = process.argv.slice(3).join(" ");
+
+  var queryURL =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
+  axios.get(queryURL).then(function(response) {
+    for (var j = 0; j < response.data.length; j++) {
+      console.log(divider);
+      console.log("");
+      console.log("Venue: " + response.data[j].venue.name);
+      console.log("");
+      console.log(
+        "Location: " +
+          response.data[j].venue.city +
+          ", " +
+          response.data[j].venue.region +
+          ", " +
+          response.data[j].venue.country
+      );
+      console.log("");
+      var datetime = response.data[j].datetime.split("T");
+      var date = moment(`${datetime[0]}`, "YYYY-MM-DD");
+      console.log("Date: " + date.format("L"));
+      console.log("");
+      console.log(divider);
+    }
+  });
+}
+
+function song() {
+  var songName = process.argv.slice(3).join(" ");
+
+  if (songName === "") {
+    songName = "The Sign";
+  }
+
+  spotify
+    .search({
+      type: "track",
+      query: songName
+    })
+    .then(function(response) {
+      console.log(divider);
+      console.log("Artist: " + response.tracks.items[0].artists[0].name);
+      console.log("");
+      console.log("Song: " + response.tracks.items[0].name);
+      console.log("");
+      console.log("Link: " + response.tracks.items[0].external_urls.spotify);
+      console.log("");
+      console.log("Album: " + response.tracks.items[0].album.name);
+      console.log(divider);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+function movie() {
+  var movieName = process.argv.slice(3).join(" ");
+
+  if (movieName === "") {
+    movieName = "Mr.Nobody";
+  }
+
+  var queryURL =
+    "http://www.omdbapi.com/?t=" +
+    movieName +
+    "&imbdRating=&Ratings=&Title=&Actors=&Country=&Language=&y=&plot=short&apikey=trilogy";
+
+  axios.get(queryURL).then(function(response) {
+    console.log(divider);
+    console.log("Movie Title: " + response.data.Title);
+    console.log("");
+    console.log("Release Year: " + response.data.Year);
+    console.log("");
+    console.log("IMBD Rating: " + response.data.imbdRating);
+    console.log("");
+    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[2].Value);
+    console.log("");
+    console.log("Country Produced In: " + response.data.Country);
+    console.log("");
+    console.log("Original Language: " + response.data.Language);
+    console.log("");
+    console.log("Movie Plot: " + response.data.Plot);
+    console.log("");
+    console.log("Actors: " + response.data.Actors);
+    console.log("");
+    console.log(divider);
+  });
+}
+
+function whatever() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) throw err;
+    var random = data.split(",")[0];
+    var rand = [data.split(",")[1].replace(/^"(.*)"$/, "$1")];
+
+    switch (random) {
+      case "concert-this":
+        concert1();
+        break;
+      case "spotify-this-song":
+        song1();
+        break;
+      case "movie-this":
+        movie1();
+        break;
+    }
+
+    function concert1() {
+      var artist = rand;
+
+      var queryURL =
+        "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp";
+      axios.get(queryURL).then(function(response) {
+        for (var j = 0; j < response.data.length; j++) {
+          console.log(divider);
+          console.log("");
+          console.log("Venue: " + response.data[j].venue.name);
+          console.log("");
+          console.log(
+            "Location: " +
+              response.data[j].venue.city +
+              ", " +
+              response.data[j].venue.region +
+              ", " +
+              response.data[j].venue.country
+          );
+          console.log("");
+          var datetime = response.data[j].datetime.split("T");
+          var date = moment(`${datetime[0]}`, "YYYY-MM-DD");
+          console.log("Date: " + date.format("L"));
+          console.log("");
+          console.log(divider);
         }
-        else {
-          artist += nodeArgs[i];      
-        }
+      });
+    }
+
+    function song1() {
+      var songName = rand;
+
+      if (songName === "") {
+        songName = "The Sign";
       }
 
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    axios.get(queryURL).then(
-        function(response){
-            for(var j = 0; j < response.data.length; j++){
-                console.log(response.data[j].venue.name);
-                console.log(response.data[j].venue.city + ", " + response.data[j].venue.region + ", " + response.data[j].venue.country);
-                var datetime = response.data[j].datetime.split("T");
-                var date = moment(`${datetime[0]}`, "YYYY-MM-DD")
-                console.log("Date:" + date.format("L"));
-                console.log(divider);
-            }
-        }
-    )
-
-};
-
-function spotify(){
-    var songName = "";
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-          songName = songName + "+" + nodeArgs[i];
-        }
-        else {
-          songName += nodeArgs[i];      
-        }
+      spotify
+        .search({
+          type: "track",
+          query: songName
+        })
+        .then(function(response) {
+          console.log(divider);
+          console.log("Artist: " + response.tracks.items[0].artists[0].name);
+          console.log("");
+          console.log("Song: " + response.tracks.items[0].name);
+          console.log("");
+          console.log(
+            "Link: " + response.tracks.items[0].external_urls.spotify
+          );
+          console.log("");
+          console.log("Album: " + response.tracks.items[0].album.name);
+          console.log(divider);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
     }
 
-    if (songName === ""){
-        songName = "The Sign";
-    }
+    function movie1() {
+      var movieName = rand;
 
-};
-
-function movie(){ 
-    var movieName = "";
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-          movieName = movieName + "+" + nodeArgs[i];
-        }
-        else {
-          movieName += nodeArgs[i];      
-        }
-    }
-
-      if(movieName ===""){
+      if (movieName === "") {
         movieName = "Mr.Nobody";
-        };
+      }
 
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&imbdRating=&Ratings=&Title=&Actors=&Country=&Language=&y=&plot=short&apikey=trilogy";
+      var queryURL =
+        "http://www.omdbapi.com/?t=" +
+        movieName +
+        "&imbdRating=&Ratings=&Title=&Actors=&Country=&Language=&y=&plot=short&apikey=trilogy";
 
-    axios.get(queryUrl).then(
-        function(response) {
-            console.log("Movie Title: " + response.data.Title)
-            console.log("Release Year: " + response.data.Year);
-            console.log("IMBD Rating: " + response.data.imbdRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[2].Value);
-            console.log("Country Produced In: " + response.data.Country)
-            console.log("Original Language: " + response.data.Language)
-            console.log("Movie Plot: " + response.data.Plot)
-            console.log("Actors: " + response.data.Actors)
-        }
-      );
-};
-
-function whatever(){
-
-};
+      axios.get(queryURL).then(function(response) {
+        console.log(divider);
+        console.log("Movie Title: " + response.data.Title);
+        console.log("");
+        console.log("Release Year: " + response.data.Year);
+        console.log("");
+        console.log("IMBD Rating: " + response.data.imbdRating);
+        console.log("");
+        console.log(
+          "Rotten Tomatoes Rating: " + response.data.Ratings[2].Value
+        );
+        console.log("");
+        console.log("Country Produced In: " + response.data.Country);
+        console.log("");
+        console.log("Original Language: " + response.data.Language);
+        console.log("");
+        console.log("Movie Plot: " + response.data.Plot);
+        console.log("");
+        console.log("Actors: " + response.data.Actors);
+        console.log("");
+        console.log(divider);
+      });
+    }
+  });
+}
